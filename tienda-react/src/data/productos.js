f73961cef0532f06187c53da8ad11c3401be1195
Jenifer Pardo <jenifertatianapardosusa@gmail.com>
2026-08-24 11:37:39 -0500

    mostrar productos con react

---
+++ tienda-react/src/data/productos.js
@@ -0,0 +1,72 @@
+export const productos = [
+    {
+        id: 1,
+        nombre: 'Mouse',
+        precio: 50000,
+        categoria: 'Perifericos',
+        stock: 5
+    },
+    {
+        id: 2,
+        nombre: 'Teclado',
+        precio: 90000,
+        categoria: 'Perifericos',
+        stock: 0
+    },
+    {
+        id: 3,
+        nombre: 'Monitor',
+        precio: 650000,
+        categoria: 'Pantallas',
+        stock: 3
+    },
+    {
+        id: 4,
+        nombre: 'Audifonos',
+        precio: 120000,
+        categoria: 'Audio',
+        stock: 8
+    },
+    {
+        id: 5,
+        nombre: 'Webcam',
+        precio: 180000,
+        categoria: 'Accesorios',
+        stock: 4
+    },
+    {
+        id: 6,
+        nombre: 'Parlantes',
+        precio: 150000,
+        categoria: 'Audio',
+        stock: 6
+    },
+    {
+        id: 7,
+        nombre: 'Memoria USB',
+        precio: 35000,
+        categoria: 'Almacenamiento',
+        stock: 10
+    },
+    {
+        id: 8,
+        nombre: 'Disco SSD',
+        precio: 280000,
+        categoria: 'Almacenamiento',
+        stock: 5
+    },
+    {
+        id: 9,
+        nombre: 'Laptop',
+        precio: 2500000,
+        categoria: 'Computadores',
+        stock: 2
+    },
+    {
+        id: 10,
+        nombre: 'Pad Mouse',
+        precio: 30000,
+        categoria: 'Perifericos',
+        stock: 12
+    }
+];

982428ad8912d4463f036955d7bb1aec0f362763
Jenifer Pardo <jenifertatianapardosusa@gmail.com>
2026-08-24 11:09:09 -0500

    commit reto 3

--- tienda-react/practica-js.js
+++ tienda-react/practica-js.js
@@ -53,19 +53,94 @@
 //console.log(resultado5);
 
 // 1. cuadrado(numero)
-const cuadrado = numero => numero * numero;
+//const cuadrado = numero => numero * numero;
 
 // 2. esMayorEdad(edad)
-const esMayorEdad = edad => edad >= 18;
+//const esMayorEdad = edad => edad >= 18;
 
 // 3. nombreCompleto(nombre, apellido)
-const nombreCompleto = (nombre, apellido) => `${nombre} ${apellido}`;
+//const nombreCompleto = (nombre, apellido) => `${nombre} ${apellido}`;
 
 
 // Ejecutar y comprobar resultados
 
-console.log("Cuadrado de 5:", cuadrado(5));
+//console.log("Cuadrado de 5:", cuadrado(5));
 
-console.log("¿Es mayor de edad?:", esMayorEdad(20));
+//console.log("¿Es mayor de edad?:", esMayorEdad(20));
 
-console.log("Nombre completo:", nombreCompleto("Diego", "Beltran"));
+//console.log("Nombre completo:", nombreCompleto("Diego", "Beltran"));
+
+
+const productos = [
+    {
+        id: 1,
+        nombre: "Mouse",
+        precio: 50000,
+        categoria: "Perifericos",
+        stock: 5
+    },
+    {
+        id: 2,
+        nombre: "Teclado",
+        precio: 90000,
+        categoria: "Perifericos",
+        stock: 0
+    },
+    {
+        id: 3,
+        nombre: "Monitor",
+        precio: 650000,
+        categoria: "Pantallas",
+        stock: 3
+    },
+    {
+        id: 4,
+        nombre: "Audifonos",
+        precio: 120000,
+        categoria: "Audio",
+        stock: 8
+    },
+    {
+        id: 5,
+        nombre: "Webcam",
+        precio: 180000,
+        categoria: "Accesorios",
+        stock: 4
+    },
+    {
+        id: 6,
+        nombre: "Parlantes",
+        precio: 150000,
+        categoria: "Audio",
+        stock: 6
+    },
+    {
+        id: 7,
+        nombre: "Memoria USB",
+        precio: 35000,
+        categoria: "Almacenamiento",
+        stock: 10
+    },
+    {
+        id: 8,
+        nombre: "Disco SSD",
+        precio: 280000,
+        categoria: "Almacenamiento",
+        stock: 5
+    },
+    {
+        id: 9,
+        nombre: "Laptop",
+        precio: 2500000,
+        categoria: "Computadores",
+        stock: 2
+    },
+    {
+        id: 10,
+        nombre: "Pad Mouse",
+        precio: 30000,
+        categoria: "Perifericos",
+        stock: 12
+    }
+];
+console.log(productos);
